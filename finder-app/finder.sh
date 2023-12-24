6518590ff82c99f78c1448e36d6b540c2883c4bf
Alex Hunsberger <aahunsberger@gmail.com>
2023-12-24 12:24:32 +0000

    assignment 1 attempt 1

---
+++ finder-app/finder.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+FILESDIR="${1}"
+SEARCHSTR="${2}"
+
+if [ -z "${FILESDIR}" ]; then
+	>&2 echo "ERROR: Argument 1 should be a path or file"
+	exit 1
+elif [ -z "${SEARCHSTR}" ]; then
+	>&2 echo "ERROR: Argument 2 should be a string to search for"
+	exit 1
+elif [ ! -d "${FILESDIR}" ]; then
+	>&2 echo "ERROR: Cannot find path or file: $1"
+	exit 1
+fi
+
+# Performance could be improved by first grepping for lines and
+# then inferring the number of files from the lines, but I've
+# prioritized code simplicity here.
+NUM_FILES=$(grep -rl "${SEARCHSTR}" "${FILESDIR}" | wc -l)
+NUM_LINES=$(grep -r "${SEARCHSTR}" "${FILESDIR}" | wc -l)
+
+echo "The number of files are ${NUM_FILES} and the number of matching lines are ${NUM_LINES}"
---
+++ finder-app/writer.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+WRITEFILE="${1}"
+WRITESTR="${2}"
+
+if [ -z "${WRITEFILE}" ]; then
+	>&2 echo "ERROR: Argument 1 should be a file"
+	exit 1
+elif [ -z "${WRITESTR}" ]; then
+	>&2 echo "ERROR: Argument 2 should be a string to write"
+	exit 1
+fi
+
+DIRNAME=$(dirname "${1}")
+if ! mkdir -p "${DIRNAME}"; then
+	>&2 echo "ERROR: failed to create ${DIRNAME}"
+	exit 1
+elif ! echo "${WRITESTR}" > "${WRITEFILE}"; then
+	>&2 echo "ERROR: failed to write ${WRITESTR} to ${WRITEFILE}"
+	exit 1
+fi
